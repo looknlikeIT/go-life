aa20b13de78d6eddbb95cd0c043e3340c01e5c61
Crush <crush@gmail.com>
2019-05-28 00:53:02 +0200

    [IMP] price_slider: transform to snippet and add color

---
+++ website_pbo/static/src/js/price_slider.js
@@ -0,0 +1,38 @@
+odoo.define('website_pbo.slider_price', function (require) {
+'use strict';
+
+var Widget = require('web.Widget');
+var websiteRootData = require('website.WebsiteRoot');
+
+var sliderPrice = Widget.extend({
+    slider: false,
+
+    start: function () {
+        var self = this;
+        var def = this._super.apply(this, arguments);
+        this.slider = document.getElementById('sliderPrice');
+
+        var conf = {
+            start: [this.$el.data('current-min'), this.$el.data('current-max')],
+            connect: true,
+            tooltips: [wNumb({decimals: 0}), wNumb({decimals: 0})],
+            range: {
+                'min': this.$el.data('slider-min'),
+                'max': this.$el.data('slider-max'),
+            }
+        }
+        noUiSlider.create(this.slider, conf);
+        this.slider.noUiSlider.on('change.one', function (o) {
+            var input = this.$el.data('input');
+            var newRange = o[0] + '-' + o[1];
+            $(input).val(newRange);
+            $(input).change()
+        });
+        return def;
+    },
+});
+
+websiteRootData.websiteRootRegistry.add(sliderPrice, '.sliderPrice');
+return LazyTemplateRenderer;
+
+})
