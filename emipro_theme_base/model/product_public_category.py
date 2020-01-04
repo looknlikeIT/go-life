1729441266f69ae643b480f7bf89e0b153cd7e1d
Crush <crush@gmail.com>
2020-01-04 13:57:58 +0100

    [FIX] emipro: product public categoy: make content unsanitized

--- emipro_theme_base/model/product_public_category.py
+++ emipro_theme_base/model/product_public_category.py
@@ -5,5 +5,5 @@ from odoo.tools.translate import html_translate
 class product_public_category(models.Model):
     _inherit = "product.public.category"
      
-    content = fields.Html('Content', translate=html_translate)
+    content = fields.Html('Content', translate=html_translate, sanitize=False)
     website_published = fields.Boolean("Website Published",default=True)
